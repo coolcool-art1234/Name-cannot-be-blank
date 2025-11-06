f8054aaefd44851f2110b9d83f302f7f8a79a7a0
coolcool-art1234 <aidan.murrowsmith@naenaeint.school.nz>
2025-11-07 08:58:52 +1300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function generateRandom() {
+    const min = parseInt(document.getElementById('min').value, 10);
+    const max = parseInt(document.getElementById('max').value, 10);
+    if (isNaN(min) || isNaN(max) || min > max) {
+        document.getElementById('result').innerText = "Please enter a valid range.";
+        return;
+    }
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById('result').innerText = "🎲 Random Number: " + random;
